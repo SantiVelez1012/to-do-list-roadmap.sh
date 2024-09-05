d31fe34c4a8d4fd370c3d215aac83fff3bb8297a
Santiago Velez <santivekez1012@gmail.com>
2024-09-05 17:06:43 -0500

    project initialization

---
+++ src/main/java/com/practice/to_do_list_roadmap_sh/ToDoListRoadmapShApplication.java
@@ -0,0 +1,13 @@
+package com.practice.to_do_list_roadmap_sh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListRoadmapShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListRoadmapShApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/to_do_list_roadmap_sh/ToDoListRoadmapShApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.to_do_list_roadmap_sh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListRoadmapShApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
