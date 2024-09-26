290f853d441b49afa3bada987cbb4942aa0203e4
Santiago Velez <santivekez1012@gmail.com>
2024-09-26 11:33:23 -0500

    finishing security config, starting to implement userInfoService

--- src/main/java/com/practice/to_do_list_roadmap_sh/config/JwtFilter.java
+++ src/main/java/com/practice/to_do_list_roadmap_sh/config/JwtFilter.java
@@ -2,8 +2,15 @@ package com.practice.to_do_list_roadmap_sh.config;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.practice.to_do_list_roadmap_sh.api.services.UserInfoService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,11 +20,45 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtFilter extends OncePerRequestFilter{
 
-    
+
+    private JwtService jwtService;
+
+    private UserInfoService userDetailsService;
+
+    JwtFilter(JwtService jwtService, UserInfoService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            try {
+                username = jwtService.extractUsername(token);
+            } catch (Exception e) {
+                logger.error("Error extrayendo el nombre del token: " + token + " : " + e.getMessage());
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido o expirado.");
+                return;
+            }
+        }
+
         filterChain.doFilter(request, response);
     }
 
